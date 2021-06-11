b756b601cd30e246099976ea90fab91a1727310a
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-11 16:39:44 +0530

    Create index.js

---
+++ symbol/index.js
@@ -0,0 +1,7 @@
+let symbol = Symbol('name');
+let otherSymbol = Symbol('name');
+
+console.log(symbol.toString()); 
+console.log(typeof symbol); 
+console.log(symbol == otherSymbol);
+console.log(symbol === otherSymbol);
